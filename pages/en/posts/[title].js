7205d1c4503f0950f49c72161b52e6d3b5f40ffe
Dvizhenia <87878210+Dvizhenia@users.noreply.github.com>
2022-01-05 20:15:30 +0400

    wd

--- pages/en/posts/[title].js
+++ pages/en/posts/[title].js
@@ -2,12 +2,13 @@ import React from 'react'
 import { useRouter } from 'next/dist/client/router'
 import Title from '../../../comp/SEO/title'
 const Single = () => {
+    
     const router = useRouter()
     const { title } = router.query
     return (
         <div>
             <Title title={title}/>
-            Wepo,{title}
+
         </div>
     )
 }
