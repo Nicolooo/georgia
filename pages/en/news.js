2b7fede6b9439d5ea6d6a30e5ec875568126d22c
Dvizhenia <87878210+Dvizhenia@users.noreply.github.com>
2022-01-05 16:49:51 +0400

    News
    as

---
+++ pages/en/news.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Title from '../../comp/SEO/title'
+
+const News = () => {
+    return (
+        <div>
+            <Title title="News"/>
+            <h1>News</h1>
+        </div>
+    )
+}
+
+export default News
