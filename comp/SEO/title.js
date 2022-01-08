1e1cd91f28b8cfdefbbe4f3c3aaab809df823004
Dvizhenia <87878210+Dvizhenia@users.noreply.github.com>
2022-01-09 01:51:00 +0400

    metas

--- comp/SEO/title.js
+++ comp/SEO/title.js
@@ -5,7 +5,6 @@ const Title = ({title, meta}) => {
         <Head>
         <title>{title}</title>
         <meta charset="UTF-8"/>
-        <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
         <meta name="description" content={meta} />
         <link rel="icon" href="../../public/icon.png" />
