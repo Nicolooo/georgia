e498c62b2067843bad2d5734a165114e0c79cbe2
Dvizhenia <87878210+Dvizhenia@users.noreply.github.com>
2022-01-09 01:53:06 +0400

    Pulss

--- pages/wp-admin.js
+++ pages/wp-admin.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import Link from 'next/link'
-import Title from '../comp/SEO/Title'
+import SEO from '../comp/SEO/title'
 const admin = () => {
     return (
         <div className="wp-admin">
-        <Title title="Wp-Admin"/>
+        <SEO title="Wp-Admin"/>
             <h1>This is built on Next.js not Wordpress </h1>
             <p>To see our techstack see more About the <Link href="https://georgianliberty.xyz"><a className="ll">Techstack</a></Link> </p>
         </div>
