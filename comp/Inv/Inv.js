7edb340f302aaaf6be89264bdc83f4da887f8abf
plolq <chigladzeoto@gmail.com>
2022-01-12 18:07:19 +0400

    fix: about us, interview
    fix: about us homepage text, interview video

--- comp/Inv/Inv.js
+++ comp/Inv/Inv.js
@@ -4,7 +4,7 @@ import Link from 'next/link'
 const Inv = (LinkF) => {
     // To do: pass link props
     return (
-        <div className="inv-wrapper">
+        <div className="inv-wrapper container-fluid">
             <h2>INTERVIEWS</h2>
             <div className="inv-video ratio">
                 <iframe width="100%" src="https://www.youtube.com/embed/R3zIJ01-Vt8" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" ></iframe>
