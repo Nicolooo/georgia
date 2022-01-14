7da7b0953061e5309708c893492bd49be8daccc3
Nicolo <71701653+Nicolooo@users.noreply.github.com>
2022-01-15 02:22:30 +0400

    wef
    wefwef

--- comp/Slider/Modal.js
+++ comp/Slider/Modal.js
@@ -1,33 +1,33 @@
 import React from 'react'
 import { Swiper,SwiperSlide } from "swiper/react";
 import "swiper/css";
-import SlideContent from '../../comp/Slider/Swiper';
+import Slides from '../../data/Slides';
+
 const Modal = ({setIsOpen}) => {
-  const json = {"name":"John", "age":30, "car":null}
     return (
     <>
       <div className="modal-darkBG" onClick={() => setIsOpen(false)} />
         <div className="modal-centered">
           <div className="modal">          
       <Swiper spaceBetween={50} slidesPerView={1} height="100%" className="mySwiper">
-      {Object.entries(json).map(([key, val], i) => (
-         <SwiperSlide key={i}>
-         <div className="modal-left">
-                 <h2>{val}</h2>
-                 <p>gandoi</p>
-                 
-                   </div>
-                 <div className="modal-right">
-                   <h2>{val}</h2>
-                   </div>
-             <div className="modalActions">
-               <div className="actionsContainer">      
-               </div>
-             </div>
-           <button className="deleteBtn" onClick={()=>setIsOpen(false)}>
-                   Close
-           </button>   
-         </SwiperSlide>
+      {Slides.map((slide) => (
+                 <SwiperSlide key={slide.id} >
+                 <div className="modal-left">
+                         <h2>{slide.name}</h2>
+                         <p>gandoi</p>
+                         
+                           </div>
+                         <div className="modal-right">
+                           <h2>{}</h2>
+                           </div>
+                     <div className="modalActions">
+                       <div className="actionsContainer">      
+                       </div>
+                     </div>
+                   <button className="deleteBtn" onClick={()=>setIsOpen(false)}>
+                           Close
+                   </button>   
+                 </SwiperSlide>
         ))}
         </Swiper>
           </div>
---
+++ data/Slides.js
@@ -0,0 +1,14 @@
+const Slides = [
+    {
+        id: 1,
+        name: "Slide 1",
+        text: "ragava"
+    },
+    {
+        id: 2,
+        name: "Slide 2",
+        text: "ragava"
+    },
+]
+
+export default Slides
