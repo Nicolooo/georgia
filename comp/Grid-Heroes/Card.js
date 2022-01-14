8dad4382dfb67d289a264b91dfbed4b0eacdab0c
Nicolo <71701653+Nicolooo@users.noreply.github.com>
2022-01-14 15:57:12 +0400

    asf

--- comp/Grid-Heroes/Card.js
+++ comp/Grid-Heroes/Card.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Card = ({name,title,img}) => {
     return (
     <div className="heroes-card">
-        <img src="https://i2.wp.com/www.georgianliberty.support/wp-content/uploads/2021/08/1992.webp?resize=1024%2C745&ssl=1"
+        <img src="https://raw.githubusercontent.com/Nicolooo/Georgianlibertycontent/main/1992.webp"
         loading="lazy" title="..." alt="..." width="684" height="742"/>
       <div className="heroes-card-text">
         <h3>{name}</h3>
