e481f456424ef91890a50f96ba542c20b2aca5f1
Nicolo <71701653+Nicolooo@users.noreply.github.com>
2022-01-14 20:53:21 +0400

    ewfr

--- comp/Inv/Inv.js
+++ comp/Inv/Inv.js
@@ -7,7 +7,7 @@ const Inv = (LinkF) => {
         <div className="inv-wrapper container-fluid">
             <h2>INTERVIEWS</h2>
             <div className="inv-video ratio">
-                <iframe width="100%" src="https://www.youtube.com/embed/R3zIJ01-Vt8" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" ></iframe>
+                {/* <iframe width="100%" src="https://www.youtube.com/embed/R3zIJ01-Vt8" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" ></iframe> */}
             </div>
             <Link href="as"><a className="inv-link btn">View All interviews</a></Link>
         </div>
