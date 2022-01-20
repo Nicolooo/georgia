9e3c0594f413d17854b7ead957007395bafc9860
Nicolo <71701653+Nicolooo@users.noreply.github.com>
2022-01-21 01:00:07 +0400

    წეფ

--- comp/Grid-Heroes/grid.js
+++ comp/Grid-Heroes/grid.js
@@ -6,12 +6,12 @@ const Hrid = () => {
         <div className="heroes-wrapper container-fluid">
             <h2 className='heroes-header'>NATIONAL HEROES</h2>
             <div className="container row">
-                <Card title="Guram Gabiskiria" title="MAYOR OF SOKHUMI" img={Storage.photos.Ggabiskria}/>
-                <Card title="GIORGI ANTSUXELIDZE" title="JUNIOR SERGEANT OF THE GEORGIAN ARMED FORCE" img={Storage.photos.Gant}/>
-                <Card title="ZHIULI SHARTAVA" title="HEAD OF THE COUNCIL OF MINISTERS OF THE AUTONOMOUS REPUBLIC OF ABKHAZIA" img={Storage.photos.shartava}/>
-                <Card title="ALEKSANDRE ONIANI" title="JUNIOR SERGEANT OF THE GEORGIAN ARMED FORCE" img={Storage.photos.oniani}/>
-                <Card title="GENO ADAMIA" title="MAJOR GENERAL OF THE GEORGIAN ARMED FORCES" img={Storage.photos.adamia}/>
-                <Card title="ZURAB IARAJULI" title="SENIOR LEUTENANT OF THE GEORGIAN AIR FORCE" img={Storage.photos.iarauli}/>
+                <Card name="Guram Gabiskiria" title="MAYOR OF SOKHUMI" img={Storage.photos.Ggabiskria}/>
+                <Card name="GIORGI ANTSUXELIDZE" title="JUNIOR SERGEANT OF THE GEORGIAN ARMED FORCE" img={Storage.photos.Gant}/>
+                <Card name="ZHIULI SHARTAVA" title="HEAD OF THE COUNCIL OF MINISTERS OF THE AUTONOMOUS REPUBLIC OF ABKHAZIA" img={Storage.photos.shartava}/>
+                <Card name="ALEKSANDRE ONIANI" title="JUNIOR SERGEANT OF THE GEORGIAN ARMED FORCE" img={Storage.photos.oniani}/>
+                <Card name="GENO ADAMIA" title="MAJOR GENERAL OF THE GEORGIAN ARMED FORCES" img={Storage.photos.adamia}/>
+                <Card name="ZURAB IARAJULI" title="SENIOR LEUTENANT OF THE GEORGIAN AIR FORCE" img={Storage.photos.iarauli}/>
             </div>
         </div>
     )
