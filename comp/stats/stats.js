518dc934a1967cd445777b1c4412886d6a668590
Nicolo <71701653+Nicolooo@users.noreply.github.com>
2022-03-04 00:40:06 +0400

    jjj

--- comp/stats/stats.js
+++ comp/stats/stats.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Count from '../../comp/Abk/counter'
 const Stats = ({title,tt}) => {
   return (
-      <div className="ll">
+    <div className="ll">
       <h3 className="stats-title">{title}</h3>
     <div className="stats row">
         {tt.counts.map(count => <Count key={count.id} data={count}/>)}
