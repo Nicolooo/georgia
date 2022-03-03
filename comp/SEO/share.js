2c7a168f87bc8eea698f1720c49b44c986bd67fc
Nicolo <71701653+Nicolooo@users.noreply.github.com>
2022-03-04 01:04:37 +0400

    ewf

--- comp/SEO/share.js
+++ comp/SEO/share.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Facebook, Twitter } from "react-feather"
 import Link from "next/link"
-const Share = ({url}) => {
+const Share = ({url,title}) => {
     const fb = "https://www.facebook.com/sharer.php?u=https%3A%2F%2Fgeorgianliberty.com"
     const tw =  "https://twitter.com/intent/tweet?url=https://georgianliberty"
     console.log(fb+url)
     return (<>
-    <h3 className="share-title">Share Article</h3>
+    <h3 className="share-title">{title}</h3>
         <div className="share-wrapper">
             <Link href={fb + url}><a className="share-icon-fb"><Facebook /></a></Link>
             <Link href={tw +url}><a className="share-icon-tw"><Twitter /></a></Link>
--- pages/ge/about.js
+++ pages/ge/about.js
@@ -17,7 +17,7 @@ const About = () => {
             <div className="container-fluid">
                 <div className="container mb-5">
                     <h1 className="abt-title mb-5" data-aos="fade-down">ჩვენ შესახებ</h1>
-                    <p className="abt-text" data-aos="fade-down">{Abt[0]}<span className="abt-red">{Abt[1]}</span></p>
+                    <p className="abt-text" data-aos="fade-down">{Abt[2]}<span className="abt-red">{Abt[3]}</span></p>
                 </div>
                 <Staff ok={pepo}/>
                 <Footer main="Stand Against Illegal occupation" copyright="Copyright 2021 © Georgian Liberty"/>
