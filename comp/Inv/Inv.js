10923103e35b72f469361c7e60305f1760507280
Nicolo <71701653+Nicolooo@users.noreply.github.com>
2022-03-05 19:10:31 +0400

    ef

--- comp/Inv/Inv.js
+++ comp/Inv/Inv.js
@@ -6,7 +6,7 @@ const Inv = ({title,ll}) => {
         <div className="inv-wrapper container-fluid">
             <h2>{title}</h2>
             <div className="inv-video ratio">
-            <iframe width="1060" height="515" src="https://www.youtube.com/embed/R3zIJ01-Vt8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <iframe width="1060" height="515" src="https://www.youtube.com/embed/R3zIJ01-Vt8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen className="video"></iframe>
             </div>
         </div>
     )
