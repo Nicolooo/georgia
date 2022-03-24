fcfa306cd7e327b1f0b40f46bd606a9c7f907958
Nicolo <71701653+Nicolooo@users.noreply.github.com>
2022-03-24 18:48:09 +0400

    wefffffff

--- pages/en/1801.js
+++ pages/en/1801.js
@@ -14,7 +14,11 @@ const pirveli = () => {
             <div className="abkhazia-title row"> 
                 <h1 className='page-header'>1801</h1>
                 <p className="timeline-shorts">
-                    {Storage[1801].text}                    
+                On July 24th, 1783, a “Friendship Agreement” known as the Treaty of Georgievsk was signed between the Kingdom of Kartli-Kakheti and the Russian Empire. According to which the Kingdom of Kartli-Kakheti came under the protection of the Russian Empire. Under the terms of the treaty, the Kingdom of Kartli-Kakheti refused to pursue an independent foreign policy, while Russia promised military assistance.
+                <div className="jigari"></div>
+                At the same time, Kartli-Kakheti maintained the status of a kingdom. It was ruled by a Georgian king, who had to be approved by the Russian imperial court. The terms of this treaty were repeatedly violated by the Russian Empire. In particular, they did not fulfill their promise and regularly left the Georgian kings face to face with the enemy. However, in 1801 they completely ignored the existence of the treaty and established a total Russian rule over the Kingdom of Kartli-Kakheti.
+                <div className="jigari"></div>
+                On September 12, 1801, the Russian Emperor Alexander I issued an order declaring the Kingdom of Kartli-Kakheti abolished and annexing it to the Russian Empire. The order became known to the Georgian public only on April 12 of the same year, when the representatives of the nobility gathered at the Zion Cathedral, which was besieged by the Russian army in Tbilisi, and stated the orders. Of course, such one-sided violence was met with great protest from the Georgian society. However, neither the nobility nor the commonality found enou                 
                 </p>
             </div>
             <Share url="/en/1801" title="Share the Article" />
--- pages/en/1918.js
+++ pages/en/1918.js
@@ -14,7 +14,11 @@ const Meore = () => {
             <div className="abkhazia-title row"> 
                 <h1 className='page-header'>1918</h1>
                 <p className="timeline-shorts">
-                    {Storage[1918].text}                    
+                After the February Revolution of 1917, Russia was governed by an interim government. By the decision of the Provisional Government, the management of the Transcaucasus was entrusted to a special committee of the Transcaucasus – Ozakom. However, Ozakom lost power after the Bolshevik Revolution in Russia in October 1917 and the overthrowing of the interim government. On November 15, 1917, Ozakom took over the functions of a new body of the regional government – The Transcaucasian Commissariat. With the establishment of the Commissariat, Transcaucasia officially separated from Soviet Russia. 
+                <div className="jigari"></div>
+                or its part, the Transcaucasian Commissariat convened the Transcaucasian Seim, which was a representative body of the Transcaucasian countries and was headed by Nikoloz Chkheidze. It was the Transcaucasian Seim that decided on April 22, 1918, announcing the creation of the Transcaucasian Democratic Federal Republic. It was an attempt to unite the three states, Georgia, Armenia, and Azerbaijan, led by Akaki Chkhenkeli. Unfortunately, for a number of internal and foreign political reasons, the federation was unable to perform its functions, and on May 26, 1918, the Transcaucasian Seim declared its self-liquidation and, with it, the liquidation of the Democratic Federal Republic.
+                <div className="jigari"></div>
+                On the same day, at 4:50 a.m. on May 26, 1918, a sitting of the Georgian National Council chaired by Noe Jordania opened at the former Palace of the Caucasus Crown Prince in Tbilisi. He addressed the audience, after which he read the “Act of Independence of Georgia.” From that moment on, Georgia declared its independence and plan to take its place in the Commonwealth of Independent States as a full-fledged, sovereign democratic republic.              
                 </p>
             </div>
             <Share url="/en/1918" title="Share the Article" />
