c93da878c7df1d038b2d6cc51993b119a0030093
Nicolo <71701653+Nicolooo@users.noreply.github.com>
2023-02-18 14:12:35 +0400

    About responsive fix

--- comp/Inv/Inv.js
+++ comp/Inv/Inv.js
@@ -6,7 +6,7 @@ const Inv = ({title,view}) => {
         <div className="inv-wrapper container-fluid">
             <h2>{title}</h2>
             <div className="inv-video ratio">
-            <iframe width="1060" height="615" src="https://www.youtube.com/embed/ETvKT5I-BXE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <iframe width="400" height="450" src="https://www.youtube.com/embed/ETvKT5I-BXE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             </div>
             <Link href="https://www.youtube.com/channel/UCUvGWCPrEt8djat0s5V4b_w"><a className="inv-more">{view}</a></Link>
         </div>
