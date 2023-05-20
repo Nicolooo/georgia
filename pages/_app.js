4f3a773ebfc345a31c67e2e0f61d999058a87511
Nicolo <71701653+Nicolooo@users.noreply.github.com>
2023-05-20 19:41:03 +0400

    KKK

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import MenuManager from '../comp/Menu/MenuManager'
 import { useEffect } from 'react';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
+
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
     AOS.init({
