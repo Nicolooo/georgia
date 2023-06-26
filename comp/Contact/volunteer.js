2a378144317b5a70358cc2777ede193eb9787aae
Dvizhenia <87878210+Dvizhenia@users.noreply.github.com>
2023-06-26 21:55:46 +0400

    Volunteers page

--- comp/Contact/volunteer.js
+++ comp/Contact/volunteer.js
@@ -98,6 +98,17 @@ const FormV = ({message,title}) => {
           placeholder="EMAIL"
           className="form-input"
         />
+         <input
+          autocomplete="false"
+          id="text"
+          type="text"
+          name="_replyto"
+          onChange={handleOnChange}
+          required={true}
+          value={inputs.phone}
+          placeholder="PHONE NUMBER"
+          className="form-input"
+        />
           <input
           autocomplete="false"
           id="fb"
@@ -111,24 +122,47 @@ const FormV = ({message,title}) => {
         />
             <fieldset id="fs-frm-selects" className="field-parent">
             <select name="city" id="city" required="true" onChange={handleOnChange} value={inputs.city} className="honeypie">
-                <option value="" selected="" disabled="">Choose</option>
-                <option value="1">Batumi</option>
-                <option value="3">Tbilisi</option>
-                <option value="5">Xobi</option>
-                <option value="7">Kobuleti</option>
-                <option value="9">Zugdidi</option>
+                <option value="" selected="" disabled="">CITY</option>
+                <option value="Batumi">Batumi</option>
+                <option value="Tbilisi">Tbilisi</option>
+                <option value="Rustavi">Rustavi</option>
+                <option value="Gori">Gori</option>
+                <option value="Telavi">Telavi</option>
+                <option value="Poti">Poti</option>
+                <option value="Kobuleti">Kobuleti</option>
+                <option value="Zugdidi">Zugdidi</option>
+                <option value="Other">Other</option>
             </select>
         </fieldset>
         <fieldset id="fs-frm-selects" className="field-parent">
             <select name="age" id="age" required="true" onChange={handleOnChange} value={inputs.age} className="honeypie">
-                <option value=""  selected="" disabled="">Age</option>
+                <option value=""  selected="" disabled="">AGE</option>
                 <option value="14-17">14-17</option>
                 <option value="18-21">18-21</option>
                 <option value="22-25">22-25</option>
                 <option value="25+">25+</option>
             </select>
         </fieldset> 
-        
+           <fieldset id="fs-frm-selects" className="field-parent">
+            <select name="type" id="type" required="true" onChange={handleOnChange} value={inputs.type} className="honeypie">
+                <option value="" selected="" disabled="">ACTIVITY</option>
+                <option value="Organisational work">Organisational work</option>
+                <option value="Event work">Event work</option>
+                <option value="Digital Media">Digital Media (Photo/Video, Montage, Graphic Design, Content Creation)</option>
+                <option value="Platform development">Platform development (Software support, Graphic Design)</option>
+                <option value="Remote Work">Remote Work</option>
+                <option value="Analytics/Research/Translation">Analytics/Research/Translation</option>
+                <option value="other">Other</option>
+            </select>
+        </fieldset>
+            <fieldset id="fs-frm-selects" className="field-parent">
+            <select name="type" id="type" required="true" onChange={handleOnChange} value={inputs.time} className="honeypie">
+                <option value="" selected="" disabled="">WORKING TIME</option>
+                <option value="Everyday">Everyday</option>
+                <option value="2-4 Working Days">2-4 Working Days</option>
+                <option value="Once a week">Once a week</option>
+            </select>
+        </fieldset>
       <button type="submit" disabled={status.submitting} className="btn con-sub pp" onClick={handleOnSubmit}>
           {!status.submitting
             ? !status.submitted
